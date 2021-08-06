3129fd6b737cd2379bc7775f6a50a470edb5affd
hamza <hamza_pisces@live.com>
2021-08-07 02:15:53 +0500

    Edit Modal Design

--- app/Http/Resources/CompanyResource.php
+++ app/Http/Resources/CompanyResource.php
@@ -17,14 +17,15 @@ class CompanyResource extends JsonResource
     public function toArray($request)
     {
         return [
+          'id'=>$this->id,
            'name'=>$this->name,
+           'legal'=>$this->name.' Inc',
            'url'=>$this->url,
            'location'=>$this->location,
            'type'=>$this->type,
-           'reg_no'=>$this->reg_no,
+           'reg_no'=>$this->rg_no,
            'about'=>Str::of($this->about)->limit(75),
-           'created_at'=>$this->created_at->diffForHumans()
-
+           'created_at'=>$this->created_at
         ];
     }
 }
